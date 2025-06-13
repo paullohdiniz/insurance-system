4cbee94df7e97867370d6521dc3c34b272bd861a
Paulo Diniz <paullodiniz@p2pinteligencia.com>
2025-06-13 09:45:26 -0300

    Initial commit

---
+++ src/main/java/br/com/nttconsult/provateste/ProvatesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.nttconsult.provateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvatesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvatesteApplication.class, args);
+	}
+
+}
